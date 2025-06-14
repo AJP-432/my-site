e85a5c6c3e087148762a1c544246cec532439e8c
Ajlal Paracha <ajlal.paracha@gmail.com>
2025-06-13 21:19:40 -0400

    chore: setup shadcn-svelte + default light and dark theming

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-
+	// shadcn-svelte dark mode
+	import { ModeWatcher } from 'mode-watcher';
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
 {@render children()}
