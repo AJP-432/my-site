ce3876719d7db69911ad48b3ecf44e295360f2b5
Ajlal Paracha <ajlal.paracha@gmail.com>
2025-06-15 08:20:37 -0400

    fix: no custom cursor on mobile devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,24 @@
 	import '../app.css';
 	// shadcn-svelte dark mode
 	import { ModeWatcher } from 'mode-watcher';
-
 	// Custom cursor; see https://www.npmjs.com/package/curseur
 	import { Cursor } from 'curseur';
+	import { browser } from '$app/environment';
+	import isMobile from 'ismobilejs';
 
 	let { children } = $props();
+
+	let isMobileDevice = $state(false);
+
+	$effect(() => {
+		if (browser) {
+			isMobileDevice = isMobile(navigator.userAgent).any;
+		}
+	});
 </script>
 
 <ModeWatcher />
-<Cursor color="white" mixBlendMode="exclusion" size={12} />
+{#if !isMobileDevice}
+	<Cursor color="white" mixBlendMode="exclusion" size={12} />
+{/if}
 {@render children()}
